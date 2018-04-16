f2ecbc1d9a377bfa53538533a11f50443336062f
= <vitorleoneloficial@gmail.com>
2018-04-16 13:44:21 -0300

    add translation for footer

--- locales/en.js
+++ locales/en.js
@@ -1,3 +1,15 @@
 module.exports = {
-  description: 'The biggest Vue.js experience in Latin America!'
+  description: 'The biggest Vue.js experience in Latin America!',
+
+  footer: {
+    description1: 'Produced and organized by the ',
+    description2: 'Brazilian community of Vue.js.',
+    contact: 'Contact',
+    links: {
+      description: 'Useful links',
+      speakers: 'Speakers',
+      schedule: 'Schedule',
+      workshops: 'Workshops'
+    }
+  }
 }
--- locales/pt.js
+++ locales/pt.js
@@ -1,3 +1,15 @@
 module.exports = {
-  description: 'A maior experiência Vue.js da america latina!'
+  description: 'A maior experiência Vue.js da america latina!',
+
+  footer: {
+    description1: 'Produzido e organizado pela ',
+    description2: 'comunidade brasileira de Vue.js.',
+    contact: 'Contato',
+    links: {
+      description: 'Links úteis',
+      speakers: 'Palestrantes',
+      schedule: 'Agenda',
+      workshops: 'Workshops'
+    }
+  }
 }
