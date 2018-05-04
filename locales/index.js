8e39675e2adb4b27e8adb6c3f640eabbb8af1c31
Igor Luiz <igor@devstacks.com.br>
2018-05-04 10:58:18 -0300

    Add iso on locales

--- locales/index.js
+++ locales/index.js
@@ -3,8 +3,8 @@ const enMessages = require('./en')
 
 module.exports = {
   locales: [
-    { code: 'pt' },
-    { code: 'en' }
+    { code: 'pt', iso: 'pt-BR' },
+    { code: 'en', iso: 'en-US' }
   ],
   defaultLocale: 'pt',
   vueI18n: {
