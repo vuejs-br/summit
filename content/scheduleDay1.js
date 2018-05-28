28d74f8f28fe7de3d0ebda58d04dc7a907103e9b
Igor Luiz <igor@devstacks.com.br>
2018-05-27 22:18:08 -0300

    Update schedule

--- content/scheduleDay1.js
+++ content/scheduleDay1.js
@@ -4,7 +4,7 @@ export default [
     // img: "/img/ic-happy.svg",
     img: "/img/ic-coffee-break.svg",
     topic: ["Recepção / Coffee time", "Reception / Coffee time"],
-    startTime: "9:00",
+    startTime: "8:00",
     duration: "30min"
   },
   {
@@ -13,18 +13,18 @@ export default [
     topic: ["Introdução", "Introduction"],
     author: ["Patrocinadores", "Sponsors"],
     authorInfo: "-----------------------------------",
-    startTime: "9:30",
+    startTime: "8:50",
     duration: "20min"
   },
-  {
-    type: "talk",
-    img: ["/img/thaissa.jpeg", "/img/igor.jpg"],
-    topic: "keynote",
-    author: "Thaíssa Candella e Igor Halfeld",
-    authorInfo: ["Organizadores", "Organizers"],
-    startTime: "9:50",
-    duration: "1h"
-  },
+  // {
+  //   type: "talk",
+  //   img: ["/img/thaissa.jpeg", "/img/igor.jpg"],
+  //   topic: "keynote",
+  //   author: "Thaíssa Candella e Igor Halfeld",
+  //   authorInfo: ["Organizadores", "Organizers"],
+  //   startTime: "9:50",
+  //   duration: "1h"
+  // },
   {
     type: "talk",
     img: "https://t3.ftcdn.net/jpg/01/04/10/10/240_F_104101070_wbEDt3CmlzqnPbdmOlVCL7Q7yu9mCduz.jpg",
@@ -38,8 +38,8 @@ export default [
     // ],
     authorInfo: ["Submissão da comunidade", "Community submission"],
     // description: "Vue is versatile. It can be dropped into a page like jQuery, or power an entirely independent frontend. These are the well-known extremes, but for many apps and teams, a lesser-known shade in between would be a better fit.\n\n In this talk, we’ll explore the entire spectrum of integration strategies, including varieties at the extremes. For every kind of app, you’ll learn what’s worked for others and what’s held them back, including specific code examples you can adapt for your own apps.",
-    startTime: "10:50",
-    duration: "45min"
+    startTime: "09:00",
+    duration: "50min"
   },
   {
     type: "talk",
@@ -55,15 +55,14 @@ export default [
     ],
     authorInfo: ["Gerard Sans", "Gerard Sans"],
     description: "A equipe do Apollo acaba de lançar a nova versão do Apollo Client. Já existe muita publicidade em torno da nova arquitetura usando uma nova abordagem modular e flexível. Nesta palestra, vamos colocá-lo para testá-lo junto com o Vue cobrindo consultas, mutações e em tempo real. Preparar-se!",
-    startTime: "11:40",
-    duration: "45min"
+    startTime: "09:50",
+    duration: "50min"
   },
   {
     type: "other",
-    img: "/img/ic-lunch-break.svg",
-    topic: ["Almoço", "Lunch"],
-    startTime: "12:25",
-    duration: "1h 25min"
+    img: "/img/ic-end-2.svg",
+    topic: ["Pausa", "Interval"],
+    startTime: "10:40"
   },
   // {
   //   type: "other",
@@ -87,8 +86,15 @@ export default [
     ],
     authorInfo: ["Erick Petrucelli", "Erick Petrucelli"],
     description: "Em um mundo baseado em componentes, por muito tempo estivemos tão focados no client-side e nossos incríveis SPA... De repente, SSR se tornou um daqueles estudos quase obrigatórios! O quanto isso é realmente relevante no contexto do Vue? O que o time principal preparou para abordar tal realidade? Como o Nuxt entra nesse contexto? E afinal, o que realmente vale a pena utilizar? Essas e outras perguntas serão alvo de reflexões e críticas durante esta talk, enquanto abordamos dicas e outras curiosidades relacionadas.",
-    startTime: "13:50",
-    duration: "45min"
+    startTime: "10:50",
+    duration: "50min"
+  },
+  {
+    type: "other",
+    img: "/img/ic-lunch-break.svg",
+    topic: ["Almoço", "Lunch"],
+    startTime: "12:25",
+    duration: "1h 20min"
   },
   // {
   //   type: "talk",
@@ -104,7 +110,7 @@ export default [
   //   authorInfo: "Desenvolvedora front-end há 5 anos - AppCívico & Eokoe.",
   //   // description: "Vue is versatile. It can be dropped into a page like jQuery, or power an entirely independent frontend. These are the well-known extremes, but for many apps and teams, a lesser-known shade in between would be a better fit.\n\n In this talk, we’ll explore the entire spectrum of integration strategies, including varieties at the extremes. For every kind of app, you’ll learn what’s worked for others and what’s held them back, including specific code examples you can adapt for your own apps.",
   //   startTime: "14:40",
-  //   duration: "45min"
+  //   duration: "50min"
   // },
   {
     type: "talk",
@@ -120,16 +126,8 @@ export default [
     ],
     authorInfo: ["Edd Yerburgh", "Edd Yerburgh"],
     description: "How to write unit tests for applications that use Vuex.",
-    startTime: "14:40",
-    duration: "45min"
-  },
-  {
-    type: "other",
-    // img: "/img/ic-happy.svg",
-    img: "/img/ic-coffee-break.svg",
-    topic: "Coffee time",
-    startTime: "15:30",
-    duration: "30min"
+    startTime: "13:00",
+    duration: "50min"
   },
   {
     type: "talk",
@@ -144,7 +142,63 @@ export default [
     ],
     authorInfo: ["Jacob Schatz", "Jacob Schatz"],
     description: "I'll talk about the internals of Vue, and like the compiler and the abstract syntax trees it generates. As well as how to use babel core with postcss and more others to do really crazy things.",
-    startTime: "16:00",
+    startTime: "13:50",
+    duration: "50min"
+  },
+  {
+    type: "talk",
+    img: "https://t3.ftcdn.net/jpg/01/04/10/10/240_F_104101070_wbEDt3CmlzqnPbdmOlVCL7Q7yu9mCduz.jpg",
+    topic: "Call4Papers",
+    author: ["Palestra", "Lecture"],
+    // social: [
+    //   {
+    //     "github": "https://github.com/chrisvfritz",
+    //     "twitter": "https://twitter.com/chrisvfritz"
+    //   }
+    // ],
+    authorInfo: ["Submissão da comunidade", "Community submission"],
+    // description: "Vue is versatile. It can be dropped into a page like jQuery, or power an entirely independent frontend. These are the well-known extremes, but for many apps and teams, a lesser-known shade in between would be a better fit.\n\n In this talk, we’ll explore the entire spectrum of integration strategies, including varieties at the extremes. For every kind of app, you’ll learn what’s worked for others and what’s held them back, including specific code examples you can adapt for your own apps.",
+    startTime: "14:40",
+    duration: "50min"
+  },
+  {
+    type: "talk",
+    img: "https://t3.ftcdn.net/jpg/01/04/10/10/240_F_104101070_wbEDt3CmlzqnPbdmOlVCL7Q7yu9mCduz.jpg",
+    topic: "Call4Papers",
+    author: ["Palestra", "Lecture"],
+    // social: [
+    //   {
+    //     "github": "https://github.com/chrisvfritz",
+    //     "twitter": "https://twitter.com/chrisvfritz"
+    //   }
+    // ],
+    authorInfo: ["Submissão da comunidade", "Community submission"],
+    // description: "Vue is versatile. It can be dropped into a page like jQuery, or power an entirely independent frontend. These are the well-known extremes, but for many apps and teams, a lesser-known shade in between would be a better fit.\n\n In this talk, we’ll explore the entire spectrum of integration strategies, including varieties at the extremes. For every kind of app, you’ll learn what’s worked for others and what’s held them back, including specific code examples you can adapt for your own apps.",
+    startTime: "15:00",
+    duration: "20min"
+  },
+  {
+    type: "other",
+    // img: "/img/ic-happy.svg",
+    img: "/img/ic-coffee-break.svg",
+    topic: "Coffee time",
+    startTime: "15:10",
+    duration: "25min"
+  },
+  {
+    type: "talk",
+    img: "https://t3.ftcdn.net/jpg/01/04/10/10/240_F_104101070_wbEDt3CmlzqnPbdmOlVCL7Q7yu9mCduz.jpg",
+    topic: "Call4Papers",
+    author: ["Palestra", "Lecture"],
+    // social: [
+    //   {
+    //     "github": "https://github.com/chrisvfritz",
+    //     "twitter": "https://twitter.com/chrisvfritz"
+    //   }
+    // ],
+    authorInfo: ["Submissão da comunidade", "Community submission"],
+    // description: "Vue is versatile. It can be dropped into a page like jQuery, or power an entirely independent frontend. These are the well-known extremes, but for many apps and teams, a lesser-known shade in between would be a better fit.\n\n In this talk, we’ll explore the entire spectrum of integration strategies, including varieties at the extremes. For every kind of app, you’ll learn what’s worked for others and what’s held them back, including specific code examples you can adapt for your own apps.",
+    startTime: "15:35",
     duration: "45min"
   },
   {
@@ -160,8 +214,24 @@ export default [
     // ],
     authorInfo: ["Submissão da comunidade", "Community submission"],
     // description: "Vue is versatile. It can be dropped into a page like jQuery, or power an entirely independent frontend. These are the well-known extremes, but for many apps and teams, a lesser-known shade in between would be a better fit.\n\n In this talk, we’ll explore the entire spectrum of integration strategies, including varieties at the extremes. For every kind of app, you’ll learn what’s worked for others and what’s held them back, including specific code examples you can adapt for your own apps.",
-    startTime: "17:45",
+    startTime: "16:20",
     duration: "45min"
+  },
+  {
+    type: "talk",
+    img: "https://t3.ftcdn.net/jpg/01/04/10/10/240_F_104101070_wbEDt3CmlzqnPbdmOlVCL7Q7yu9mCduz.jpg",
+    topic: "Call4Papers",
+    author: ["Palestra", "Lecture"],
+    // social: [
+    //   {
+    //     "github": "https://github.com/chrisvfritz",
+    //     "twitter": "https://twitter.com/chrisvfritz"
+    //   }
+    // ],
+    authorInfo: ["Submissão da comunidade", "Community submission"],
+    // description: "Vue is versatile. It can be dropped into a page like jQuery, or power an entirely independent frontend. These are the well-known extremes, but for many apps and teams, a lesser-known shade in between would be a better fit.\n\n In this talk, we’ll explore the entire spectrum of integration strategies, including varieties at the extremes. For every kind of app, you’ll learn what’s worked for others and what’s held them back, including specific code examples you can adapt for your own apps.",
+    startTime: "17:07",
+    duration: "50min"
   },
   {
     type: "other",
